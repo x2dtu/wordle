2ddbbbd3ae3c0813346cdfd8264504a1055d26ad
michaelga <michaelga@vt.edu>
2023-01-08 01:25:39 -0500

    Removed empty file and organized constants

---
+++ constants.go
@@ -0,0 +1,12 @@
+package main
+
+const WORD_LEN = 5
+const NUM_TRIES = 6
+const WORD_START = 8
+const SPACE = "        "
+const GREEN = "\u001b[32m"
+const YELLOW = "\u001b[33m"
+const RED = "\u001b[31m"
+const BLUE = "\u001b[34m"
+const CYAN = "\u001b[36m"
+const RESET = "\u001b[0m"
